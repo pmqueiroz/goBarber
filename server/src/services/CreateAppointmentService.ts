f17be7af222cb725492c6533675e0aaa0293a3ab
pmqueiroz <pedroqueiroz593@gmail.com>
2020-08-25 14:41:47 -0300

    Chore: Separation of Concerns

---
+++ server/src/services/CreateAppointmentService.ts
@@ -0,0 +1,40 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/Appointments';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface RequestDTO {
+	provider: string;
+	date: Date;
+}
+
+class CreateAppointmentService {
+	private appointmentsRepository: AppointmentsRepository;
+
+	constructor(appointmentsRepository: AppointmentsRepository) {
+		this.appointmentsRepository = appointmentsRepository;
+	}
+
+	public execute({ date, provider }: RequestDTO): Appointment {
+		const appointmentDate = startOfHour(date);
+
+		const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+			appointmentDate,
+		);
+
+		if (findAppointmentInSameDate) {
+			throw Error('This appointment is already booked');
+			// return response
+			// 	.status(400)
+			// 	.json({ message: 'This appointment is already booked' });
+		}
+
+		const appointment = this.appointmentsRepository.create({
+			provider,
+			date: appointmentDate,
+		});
+
+		return appointment;
+	}
+}
+
+export default CreateAppointmentService;
