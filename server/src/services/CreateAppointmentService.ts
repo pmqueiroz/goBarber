e2fa1fac920ccb6cf0af36f885526c73e01b63d3
pmqueiroz <pedroqueiroz593@gmail.com>
2020-08-28 13:56:41 -0300

    Chore: User password encriptation and auth

--- server/src/services/CreateAppointmentService.ts
+++ server/src/services/CreateAppointmentService.ts
@@ -4,12 +4,15 @@ import Appointment from '../models/Appointments';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface RequestDTO {
-	provider: string;
+	provider_id: string;
 	date: Date;
 }
 
 class CreateAppointmentService {
-	public async execute({ date, provider }: RequestDTO): Promise<Appointment> {
+	public async execute({
+		date,
+		provider_id,
+	}: RequestDTO): Promise<Appointment> {
 		const appointmentsRepository = getCustomRepository(
 			AppointmentsRepository,
 		);
@@ -25,7 +28,7 @@ class CreateAppointmentService {
 		}
 
 		const appointment = appointmentsRepository.create({
-			provider,
+			provider_id,
 			date: appointmentDate,
 		});
 
