1b8fcc16f14f0e92376ec1d84a31254d7afad1ce
pmqueiroz <pedroqueiroz593@gmail.com>
2020-08-29 10:09:52 -0300

    Chore: JWT token authentication

---
+++ server/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+	jwt: {
+		secret: '1bb9d69db46591c2709507e0f216114c',
+		expiresIn: '1d',
+	},
+};
--- server/src/services/AuthenticateUserService.ts
+++ server/src/services/AuthenticateUserService.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -8,8 +10,13 @@ interface Request {
 	password: string;
 }
 
+interface Response {
+	user: User;
+	token: string;
+}
+
 export default class AuthenticateUserService {
-	public async execute({ email, password }: Request): Promise<{ user: User }> {
+	public async execute({ email, password }: Request): Promise<Response> {
 		const usersRepository = getRepository(User);
 
 		const user = await usersRepository.findOne({ where: { email } });
@@ -24,8 +31,16 @@ export default class AuthenticateUserService {
 			throw new Error('Incorrect email/password combination.');
 		}
 
+		const { secret, expiresIn } = authConfig.jwt;
+
+		const token = sign({}, secret, {
+			subject: user.id,
+			expiresIn,
+		});
+
 		return {
 			user,
+			token,
 		};
 	}
 }
