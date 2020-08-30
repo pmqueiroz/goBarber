17936b207a71c60717c1ba711fbf808f2711677a
pmqueiroz <pedroqueiroz593@gmail.com>
2020-08-30 09:41:20 -0300

    Chore: Error Handling

--- server/src/services/CreateAppointmentService.ts
+++ server/src/services/CreateAppointmentService.ts
@@ -3,6 +3,8 @@ import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointments';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
+import AppError from '../errors/AppError';
+
 interface RequestDTO {
 	provider_id: string;
 	date: Date;
@@ -24,7 +26,7 @@ class CreateAppointmentService {
 		);
 
 		if (findAppointmentInSameDate) {
-			throw Error('This appointment is already booked');
+			throw new AppError('This appointment is already booked');
 		}
 
 		const appointment = appointmentsRepository.create({
