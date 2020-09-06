781b9b1b10bf6744afc70cc4bacf46623e9b4457
pmqueiroz <pedroqueiroz593@gmail.com>
2020-09-06 13:38:37 -0300

    Incomplete: routes with authentication

---
+++ web/src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+	return (
+		<Container>
+			<h1>Salve</h1>
+		</Container>
+	);
+};
+
+export default Dashboard;
---
+++ web/src/routes/Route.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+	Route as ReactDOMRoute,
+	RouteProps as ReactDomRouteProps,
+	Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDomRouteProps {
+	isPrivate?: boolean;
+	component: React.ComponentType;
+}
+
+const Route: React.FunctionComponent<RouteProps> = ({
+	isPrivate = false,
+	component: Component,
+	...rest
+}) => {
+	const { user } = useAuth();
+
+	return (
+		<ReactDOMRoute
+			{...rest}
+			render={() => {
+				return isPrivate === !!user ? (
+					<Component />
+				) : (
+					<Redirect to={{ pathname: isPrivate ? '/' : '/dashboard' }} />
+				);
+			}}
+		/>
+	);
+};
+
+export default Route;
