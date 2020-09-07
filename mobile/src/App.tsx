7dfcfdbdab1d83617b23178a757312752f42d7d3
pmqueiroz <pedroqueiroz593@gmail.com>
2020-09-07 09:27:28 -0300

    Create: mobile app init

---
+++ mobile/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FunctionComponent = () => (
+  <View>
+   <Text>Hello World</Text>
+  </View>
+);
+
+export default App;
