9fe123f0ba264fe88662656120dd4bd0e8fec4ee
pmqueiroz <pedroqueiroz593@gmail.com>
2020-09-08 19:26:24 -0300

    Chore: login page front finished

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -3,13 +3,21 @@ import { Image } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 
 const SignIn: React.FunctionComponent = () => (
   <Container>
     <Image source={logoImg} />
 
-    <Title>Log in</Title>
+    <Title>Welcome Back</Title>
+
+    <Input name="email" icon="mail" placeholder="E-mail" />
+    <Input name="password" icon="lock" placeholder="Password" />
+
+    <Button onPress={() => { console.log('deu'); }}>Log in</Button>
   </Container>
 );
 
