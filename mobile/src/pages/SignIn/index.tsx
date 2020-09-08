a34a5b1695c0428227123db2ca18cd0a64bc4be2
pmqueiroz <pedroqueiroz593@gmail.com>
2020-09-08 20:43:07 -0300

    Incomplete: login screen

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
 import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FunctionComponent = () => (
-  <Container>
-    <Image source={logoImg} />
+  <>
+    <Container>
+      <Image source={logoImg} />
 
-    <Title>Welcome Back</Title>
+      <Title>Welcome Back</Title>
 
-    <Input name="email" icon="mail" placeholder="E-mail" />
-    <Input name="password" icon="lock" placeholder="Password" />
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Password" />
 
-    <Button onPress={() => { console.log('deu'); }}>Log in</Button>
-  </Container>
+      <Button onPress={() => { console.log('deu'); }}>Log in</Button>
+
+      <ForgotPassword>
+        <ForgotPasswordText onPress={() => { console.log('deu'); }}>
+          Forgot my password
+        </ForgotPasswordText>
+      </ForgotPassword>
+
+    </Container>
+
+    <CreateAccountButton>
+      <Icon name="log-in" size={20} color="#ff9000" />
+      <CreateAccountButtonText>
+        Create Account
+      </CreateAccountButtonText>
+    </CreateAccountButton>
+  </>
 );
 
 export default SignIn;
--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -13,3 +13,21 @@ export const Title = styled.Text`
 	font-family: 'RobotoSlab-Medium';
 	margin: 64px 0 24px;
 `;
+
+export const ForgotPassword = styled.TouchableOpacity`
+	margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+	color: #f4ede8;
+	font-size: 16px;
+	font-family: 'RobotoSlab-Regular';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+
+`;
+
+export const CreateAccountButtonText = styled.Text`
+
+`;
